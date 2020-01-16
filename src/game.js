b81a80093b99b18cf976f8179f7702aca5992c5d
Kapatych <onetruestory@ya.ru>
2020-01-16 16:23:26 +0300

    Add class Game

---
+++ src/game.js
@@ -0,0 +1,115 @@
+export default class Game {
+    score = 0;
+    lines = 0;
+    level = 0;
+    playfield = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+    activePiece = {
+        x: 0,
+        y: 0,
+        blocks: [
+            [0, 1, 0],
+            [1, 1, 1],
+            [0, 0, 0]
+        ]
+    };
+
+    movePieceLeft() {
+        this.activePiece.x -= 1;
+
+        if (this.hasCollision()) {
+            this.activePiece.x += 1;
+        }
+    }
+
+    movePieceRight() {
+        this.activePiece.x += 1;
+
+        if (this.hasCollision()) {
+            this.activePiece.x -= 1;
+        }
+    }
+
+    movePieceDown() {
+        this.activePiece.y += 1;
+
+        if (this.hasCollision()) {
+            this.activePiece.y -= 1;
+            this.lockPiece();
+        }
+    }
+
+    rotatePiece() {
+        const blocks = this.activePiece.blocks;
+        const length = blocks.length;
+
+        const temp = [];
+        for (let i = 0; i < length; i++) {
+            temp[i] = new Array(length).fill(0)
+        }
+
+        for (let y = 0; y < length; y++) {
+            for (let x = 0; x < length; x++) {
+                temp[x][y] = blocks[length - 1 - y][x];
+            }
+        }
+
+        this.activePiece.blocks = temp;
+
+        if (this.hasCollision()) {
+            console.log('collision')
+            this.activePiece.blocks = blocks;
+        }
+    }
+
+    hasCollision() {
+        const {y: pieceY, x: pieceX, blocks} = this.activePiece;
+
+        for (let y = 0; y < blocks.length; y++) {
+            for (let x = 0; x < blocks[y].length; x++) {
+                if (
+                    blocks[y][x] &&
+                    ((this.playfield[pieceY + y] === undefined || this.playfield[pieceY + y][pieceX + x] === undefined) ||
+                        this.playfield[pieceY + y][pieceX + x])
+                ) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    lockPiece() {
+        const {y: pieceY, x: pieceX, blocks} = this.activePiece;
+
+        for (let y = 0; y < blocks.length; y++) {
+            for (let x = 0; x < blocks[y].length; x++) {
+                if (blocks[y][x]) {
+                    this.playfield[pieceY + y][pieceX + x] = blocks[y][x];
+                }
+            }
+        }
+
+    }
+}
