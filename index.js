b4bf65019ed161f943276e10b75617138c81a770
Kapatych <onetruestory@ya.ru>
2020-01-18 16:10:52 +0300

    Add class View

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-import Game from './src/game.js'
+import Game from './src/game.js';
+import View from './src/view.js';
+
+const root = document.querySelector('#root');
 
 const game = new Game();
+const view = new View(root, 480, 640, 20, 10);
 
 window.game = game;
+window.view = view;
+
+document.addEventListener("keydown", event => {
+    switch (event.key) {
+        case "Down": // IE/Edge specific value
+        case "ArrowDown":
+            game.movePieceDown();
+            view.render(game.getState());
+            break;
+        case "Up": // IE/Edge specific value
+        case "ArrowUp":
+            game.rotatePiece();
+            view.render(game.getState());
+            break;
+        case "Left": // IE/Edge specific value
+        case "ArrowLeft":
+            game.movePieceLeft();
+            view.render(game.getState());
+            break;
+        case "Right": // IE/Edge specific value
+        case "ArrowRight":
+            game.movePieceRight();
+            view.render(game.getState());
+            break;
+    }
+});
--- src/game.js
+++ src/game.js
@@ -1,38 +1,134 @@
 export default class Game {
+
+    static points = {
+        '1': 40,
+        '2': 100,
+        '3': 300,
+        '4': 1200
+    };
+
     score = 0;
     lines = 0;
-    level = 0;
-    playfield = [
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    ];
-    activePiece = {
-        x: 0,
-        y: 0,
-        blocks: [
-            [0, 1, 0],
-            [1, 1, 1],
-            [0, 0, 0]
-        ]
-    };
+    playfield = this.createPlayfield(20, 10);
+    activePiece = this.createPiece();
+    nextPiece = this.createPiece();
+
+    get level () {
+        return Math.floor( this.lines * 0.1 )
+    }
+
+    getState() {
+        const playfield = this.createPlayfield(20, 10);
+        const {y: pieceY, x: pieceX, blocks} = this.activePiece;
+
+        // TODO: why is this cycle?
+        for (let y = 0; y < this.playfield.length; y++) {
+            //playfield[y] = [];
+
+            for (let x = 0; x < this.playfield[y].length; x++) {
+                playfield[y][x] = this.playfield[y][x];
+            }
+        }
+
+        for (let y = 0; y < blocks.length; y++) {
+            for (let x = 0; x < blocks[y].length; x++) {
+                if (blocks[y][x]) {
+                    playfield[pieceY + y][pieceX + x] = blocks[y][x];
+                }
+            }
+        }
+
+        return {
+            score: this.score,
+            level: this.level,
+            lines: this.lines,
+            nextPiece: this.nextPiece,
+            playfield
+        };
+
+    }
+
+    createPlayfield (rows, columns) {
+        const playfield = [];
+
+        for (let y = 0; y < rows; y++) {
+            playfield[y] = [];
+
+            for (let x = 0; x < columns; x++) {
+                playfield[y][x] = 0
+            }
+        }
+
+        return playfield;
+    }
+
+    createPiece () {
+        const index = Math.floor( Math.random() * 7 );
+        const type = 'IJLOSTZ'[index];
+        const piece = {};
+
+        switch (type) {
+            case 'I':
+                piece.blocks = [
+                    [0,0,0,0],
+                    [1,1,1,1],
+                    [0,0,0,0],
+                    [0,0,0,0]
+                ];
+                break;
+            case 'J':
+                piece.blocks = [
+                    [0,0,0],
+                    [2,2,2],
+                    [0,0,2],
+                ];
+                break;
+            case 'L':
+                piece.blocks = [
+                    [0,0,0],
+                    [3,3,3],
+                    [3,0,0],
+                ];
+                break;
+            case 'O':
+                piece.blocks = [
+                    [0,0,0,0],
+                    [0,4,4,0],
+                    [0,4,4,0],
+                    [0,0,0,0]
+                ];
+                break;
+            case 'S':
+                piece.blocks = [
+                    [0,0,0],
+                    [0,5,5],
+                    [5,5,0],
+                ];
+                break;
+            case 'T':
+                piece.blocks = [
+                    [0,0,0],
+                    [6,6,6],
+                    [0,6,0],
+                ];
+                break;
+            case 'Z':
+                piece.blocks = [
+                    [0,0,0],
+                    [7,7,0],
+                    [0,7,7],
+                ];
+                break;
+            default:
+                throw new Error('unknown type of figure!')
+        }
+
+        piece.x = Math.floor( (10 - piece.blocks[0].length) /2 );
+        piece.y = -1;
+
+
+        return piece;
+    }
 
     movePieceLeft() {
         this.activePiece.x -= 1;
@@ -56,6 +152,9 @@ export default class Game {
         if (this.hasCollision()) {
             this.activePiece.y -= 1;
             this.lockPiece();
+            const clearedLines = this.clearLines();
+            this.updateScore(clearedLines);
+            this.updatePieces();
         }
     }
 
@@ -77,7 +176,6 @@ export default class Game {
         this.activePiece.blocks = temp;
 
         if (this.hasCollision()) {
-            console.log('collision')
             this.activePiece.blocks = blocks;
         }
     }
@@ -110,6 +208,48 @@ export default class Game {
                 }
             }
         }
+    }
 
+    clearLines () {
+        const rows = 20;
+        const columns = 10;
+        let lines = [];
+
+        for (let y = rows - 1; y >= 0; y--) {
+            let quantityOfBlocks = 0;
+
+            for (let x = 0; x < columns; x++) {
+                if (this.playfield[y][x]) {
+                    quantityOfBlocks += 1;
+                }
+            }
+
+            if (!quantityOfBlocks) {
+                break;
+            } else if (quantityOfBlocks === columns) {
+                lines.unshift(y);
+            } /*else {
+                continue;
+            }*/
+        }
+
+        for (let index of lines) {
+            this.playfield.splice(index, 1);
+            this.playfield.unshift( new Array(columns).fill(0))
+        }
+
+        return lines.length;
+    }
+
+    updatePieces() {
+        this.activePiece = this.nextPiece;
+        this.nextPiece = this.createPiece();
+    }
+
+    updateScore (clearedLines) {
+        if (clearedLines > 0) {
+            this.score += Game.points[clearedLines] * (this.level + 1);
+            this.lines += clearedLines;
+        }
     }
 }
